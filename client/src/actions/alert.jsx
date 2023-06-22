b641671c2b19a6406b8295f14e35870bb23a7a9f
MadhavaRaoKolla <121717739+MadhavaRaoKolla@users.noreply.github.com>
2023-06-22 22:50:35 +0530

    alert is done

--- client/src/actions/alert.jsx
+++ client/src/actions/alert.jsx
@@ -1,6 +1,6 @@
 import { SET_ALERT,REMOVE_ALERT } from "./types";
 import uuid from 'uuid';
-export const setAlert=()=>dispatch => {
+export const setAlert=(msg,alertType)=>dispatch => {
     const id=uuid.v4();
     dispatch({
         type: SET_ALERT,
