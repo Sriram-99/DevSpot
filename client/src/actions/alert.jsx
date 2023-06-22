55e270b61319c150591f4116ce7bb89a0749f87a
MadhavaRaoKolla <121717739+MadhavaRaoKolla@users.noreply.github.com>
2023-06-22 23:52:53 +0530

    small changes

--- client/src/actions/alert.jsx
+++ client/src/actions/alert.jsx
@@ -1,7 +1,7 @@
 import { SET_ALERT,REMOVE_ALERT } from "./types";
 // import uuid from "uuid";
 import { v4 as uuidv4 } from 'uuid';
-export const setAlert=(msg,alertType)=>dispatch => {
+export const setAlert=(msg,alertType,timeout=5000)=>dispatch => {
     /// uuid generates random universal id;
     const id=uuidv4();
     // const id=uuid.v4();
@@ -10,6 +10,6 @@ export const setAlert=(msg,alertType)=>dispatch => {
         payload:{msg, alertType,id}
     });
 
-   setTimeout(()=> dispatch({type:REMOVE_ALERT,payload:id}),5000);
+   setTimeout(()=> dispatch({type:REMOVE_ALERT,payload:id}),timeout);
 };
 
